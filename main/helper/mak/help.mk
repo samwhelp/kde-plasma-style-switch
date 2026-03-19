9812c37eeac03ec985e46b73c9a4ba751baff2b1
samwhelp <samwhelp@users.noreply.github.com>
2026-03-19 14:08:59 +0800

    fix

--- main/helper/mak/help.mk
+++ main/helper/mak/help.mk
@@ -15,9 +15,14 @@ help:
 	@echo '	$$ make help'
 	@echo
 	@echo
-	@echo '	$$ make Style-Colloid-Dark'
-	@echo
+	@echo '	$$ make Style-Vimix-Dark'
+	@echo '	$$ make Style-Orchis-Dark'
 	@echo '	$$ make Style-Graphite-Dark'
+	@echo '	$$ make Style-Jasper-Dark'
+	@echo '	$$ make Style-Colloid-Dark'
+	@echo '	$$ make Style-Fluent-Dark'
+	@echo '	$$ make Style-Layan-Dark'
+	@echo '	$$ make Style-Lavanda-Sea-Dark'
 	@echo
 	@echo
 .PHONY: help
